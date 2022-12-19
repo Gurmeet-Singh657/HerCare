ddeba2276004d314a3a09ccb460530027426bc60
Gurmeet <gurmeetsingh222003@gmail.com>
2022-12-20 00:43:03 +0530

    link changed

--- client/src/components/chart/ViolenceWiseChart.jsx
+++ client/src/components/chart/ViolenceWiseChart.jsx
@@ -43,7 +43,7 @@ const renderCustomizedLabel = ({
     );
 };
 export default function ViolenceWiseChart() {
-    const { data, loading, reFetch } = useFetch('hhttps://hercare-0nh9.onrender.com/getIncidentFormData')
+    const { data, loading, reFetch } = useFetch('https://hercare-0nh9.onrender.com/getIncidentFormData')
     const ViolenceData = []
     const Violence = new Map([
         ["Physical Assault", 0],
